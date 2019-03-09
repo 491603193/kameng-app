f241e80ad6772f28e2e8153f1307430f09044c0c
李竟瑞 <1076492915@qq.com>
2019-03-09 17:53:56 +0800

    17.53

--- widget/script/index/index_frm.js
+++ widget/script/index/index_frm.js
@@ -13,6 +13,17 @@ var slide3 = new auiSlide({
     "pageStyle":'dot',
     'dotPosition':'center'
 })
+var slide4 = new auiSlide({
+    container:document.getElementById("aui-slide4"),
+    // "width":300,
+    "height": 'auto',
+    "speed":500,
+    "autoPlay": 3000, //自动播放
+    "loop":true,
+    "pageShow":true,
+    "pageStyle":'dot',
+    'dotPosition':'center'
+})
 function openDetail () {
   $api.openWin('/html/classify/product_detail')
 }

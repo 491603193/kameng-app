9ca863039305875d15c2cb18be0abcf764ceeed1
衣永康 <yiyk@haitao-tech.com>
2019-03-30 17:05:19 +0800

    云仓

--- widget/script/api-ajax.js
+++ widget/script/api-ajax.js
@@ -6,9 +6,13 @@
 
   var u = {};
 
-  u.kameng_api = 'http://192.168.0.199:8031';
+  // u.kameng_api = 'http://192.168.0.199:8031';
+  // u.kameng_image = 'http://res.qiangdaoapp.com/kameng';
+  // u.kameng_web = 'http://192.168.0.199:8081';
+
+  u.kameng_api = 'http://121.42.157.174:8195/kameng-app';
   u.kameng_image = 'http://res.qiangdaoapp.com/kameng';
-  u.kameng_web = 'http://192.168.0.199:8081';
+  u.kameng_web = 'http://121.42.157.174:8195/kamneg-web';
 
   var auiToast = function() {};
   auiToast.prototype = {
